2c31ead7ec15e38978fcb87c2b63ddd8f4689a7b
ankit <1994constant@gmail.com>
2020-06-16 13:40:21 +0530

    Update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 /*!
-This crate provides the `Ini` struct which implements a basic configuration language which provides a structure similar to what’s found in Windows' `ini` files.
-You can use this to write Rust programs which can be customized by end users easily.
+This crate provides the `ini!` macro which implements a basic configuration language which provides a structure similar to what’s found in Windows' `ini` files. You can use this to write Rust programs which can be customized by end users easily.
 
-This is a simple configuration parsing utility with no dependencies built on Rust. It is inspired by Python's `configparser`.
-
-The current release is experimental, this means that future releases will be swift until we reach `stable` (1.0.0).
-The codebase is thus subject to change for now.
+This is a simple macro utility built on top of `configparser` with no other dependencies built on Rust. For more advanced functions, you should use the [configparser](https://crates.io/crates/configparser) crate.
 
 ## Quick Start
 
@@ -22,19 +18,48 @@ nuclear launch codes = topsecret
 [github.com]
 User = QEDK
 ```
-Essentially, the syntax consists of sections, each of which can which contains keys with values. The `Ini` struct can read and write such values.
+Essentially, the syntax consists of sections, each of which can which contains keys with values.
 
-## Supported datatypes
-`configparser` does not guess the datatype of values in configuration files and stores everything as strings. However, some datatypes are so common
-that it's a safe bet that some values need to be parsed in other types. For this, the `Ini` struct provides easy functions like `getint()`, `getuint()`,
-`getfloat()` and `getbool()`. The only bit of extra magic involved is that the `getbool()` function will treat boolean values case-insensitively (so
-`true` is the same as `True` just like `TRUE`). You can ofcourse just choose to parse the string values yourself.
-```ignore,rust
-let my_string = String::from("1984");
-let my_int = my_string.parse::<i32>().unwrap();
-let my_value = config.getint("somesection", "someintvalue")?.unwrap();
+### Installation
+You can install this easily via `cargo` by including it in your `Cargo.toml` file like:
+```TOML
+[dependencies]
+ini = "1.0.0"
 ```
 
+### The `ini!` macro
+The `ini!` macro allows you to simply get a hashmap for a list of files. It is planned to provide shell expansion and file-writing in the future:
+```ignore,rust
+#[macro_use]
+extern crate ini;
+
+fn main() {
+  let map = ini!("...path/to/file");
+  // Proceed to use normal HashMap functions on the map:
+  let val = map["section"]["key"].clone().unwrap();
+  // The type of the map is HashMap<String, HashMap<String, Option<String>>>
+
+  // To load multiple files, just do:
+  let (map1, map2, map3) = ini!("path/to/file1", "path/to/file2", "path/to/file3");
+  // Each map is a cloned hashmap with no relation to other ones
+}
+```
+If loading a file fails or the parser is unable to parse the file, the code will `panic` with an appropriate error. In case, you want to handle this
+gracefully, it's recommended you use the `safe` metavariable instead. This will make sure your code does not panic and instead exists as a
+`Result<HashMap, String>` type and let you deal with errors gracefully.
+```ignore,rust
+let map = ini!(safe "...path/to/file");
+// Proceed to use normal HashMap functions on the map:
+let val = map.unwrap()["section"]["key"].clone().unwrap();
+// Note the extra unwrap here, which is required because our HashMap is inside a Result type.
+```
+
+## Supported datatypes
+`configparser` does not guess the datatype of values in configuration files and stores everything as strings, same applies to `ini`. If you need getters that parse the values for you, you might want to use `configparser`. You can ofcourse just choose to parse the string values yourself.
+```ignore,rust
+let my_string = map["section"]["key"].clone().unwrap();
+let my_int = my_string.parse::<i32>().unwrap();
+```
 
 ## Supported `ini` file structure
 A configuration file can consist of sections, each led by a `[section-name]` header, followed by key-value entries separated by a `=`. By default, section names and key names are case-insensitive. All leading and trailing whitespace is removed from stored keys, values and section names.
@@ -64,71 +89,63 @@ integers, floats and booleans are held as= strings
 a_valueless_key_has_None
 this key has an empty string value has Some("") =
 
-	[indented sections]
-		can_values_be_as_well = True
-		purpose = formatting for readability
-		is_this_same     =        yes
-			is_this_same=yes
+[indented sections]
+can_values_be_as_well = True
+purpose = formatting for readability
+is_this_same     =        yes
+is_this_same=yes
 ```
 An important thing to note is that values with the same keys will get updated, this means that the last inserted key (whether that's a section header
 or property key) is the one that remains in the `HashMap`.
 The only bit of magic the API does is the section-less properties are put in a section called "default". It is planned to allow configuring this variable.
-
-## Usage
-Let's take another simple `ini` file and talk about working with it:
-```INI
-[topsecret]
-KFC = the secret herb is orega-
-
-[values]
-Int = -31415
-```
-If you read the above sections carefully, you'll know that 1) all the keys are stored in lowercase, 2) `get()` can make access in a case-insensitive
-manner and 3) we can use `getint()` to parse the `Int` value into an `i64`. Let's see that in action.
-
-```rust
-use configparser::ini::Ini;
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-  let mut config = Ini::new();
-
-  // You can easily load a file to get a clone of the map:
-  let map = config.load("tests/test.ini")?;
-  println!("{:?}", map);
-  // You can also safely not store the reference and access it later with get_map_ref() or get a clone with get_map()
-
-  // You can then access it like a normal hashmap:
-  let innermap = map["topsecret"].clone(); // Remember this is a hashmap!
-
-  // If you want to access the value, then you can simply do:
-  let val = map["topsecret"]["kfc"].clone().unwrap();
-  // Lowercasing when accessing map directly is important because all keys are stored in lower-case!
-  // Note: The .clone().unwrap() is required because it's an Option<String> type.
-
-  assert_eq!(val, "the secret herb is orega-"); // value accessible!
-
-  // What if you want to mutate the parser and remove KFC's secret recipe? Just use get_mut_map():
-  let mut_map = config.get_mut_map();
-  mut_map.get_mut("topsecret").unwrap().insert(String::from("kfc"), None);
-  // And the secret is back in safety, remember that these are normal HashMap functions chained for convenience.
-
-  // However very quickly see how that becomes cumbersome, so you can use the handy get() function from Ini
-  // The get() function accesses the map case-insensitively, so you can use uppercase as well:
-  let val = config.get("topsecret", "KFC"); // unwrapping will be an error because we just emptied it!
-  assert_eq!(val, None); // as expected!
-
-  // What if you want to get a number?
-  let my_number = config.getint("values", "Int")?.unwrap();
-  assert_eq!(my_number, -31415); // and we got it!
-  // The Ini struct provides more getters for primitive datatypes.
-
-  Ok(())
-}
-```
 */
 pub use configparser;
+use std::collections::HashMap;
 
+///The `ini!` macro allows you to simply get a hashmap for a list of files. It is planned to provide shell expansion and file-writing in the future:
+///```ignore,rust
+///#[macro_use]
+///extern crate ini;
+///
+///fn main() {
+///  let map = ini!("...path/to/file");
+///  // Proceed to use normal HashMap functions on the map:
+///  let val = map["section"]["key"].clone().unwrap();
+///  // The type of the map is HashMap<String, HashMap<String, Option<String>>>
+///
+///  // To load multiple files, just do:
+///  let (map1, map2, map3) = ini!("path/to/file1", "path/to/file2", "path/to/file3");
+///  // Each map is a cloned hashmap with no relation to other ones
+///}
+///```
+///If loading a file fails or the parser is unable to parse the file, the code will `panic` with an appropriate error. In case, you want to handle this
+///gracefully, it's recommended you use the `safe` metavariable instead. This will make sure your code does not panic and instead exists as a
+///`Result<HashMap, String>` type and let you deal with errors gracefully.
+///```ignore,rust
+///let map = ini!(safe "...path/to/file");
+///// Proceed to use normal HashMap functions on the map:
+///let val = map.unwrap()["section"]["key"].clone().unwrap();
+///// Note the extra unwrap here, which is required because our HashMap is inside a Result type.
+///```
+#[macro_export]
 macro_rules! ini {
-	{$path:literal}
+	{$($path:literal),+} => {{
+		($($crate::macro_load($path)),+)
+	}};
+	{safe $($path:literal),+} => {{
+		($($crate::macro_safe_load($path)),+)
+	}};
+}
+
+pub fn macro_load(path: &str) -> HashMap<String, HashMap<String, Option<String>>> {
+	let mut config = configparser::ini::Ini::new();
+	match config.load(path) {
+		Err(why) => panic!("{}", why),
+		Ok(map) => map
+	}
+}
+
+pub fn macro_safe_load(path: &str) -> Result<HashMap<String, HashMap<String, Option<String>>>, String> {
+	let mut config = configparser::ini::Ini::new();
+	config.load(path)
 }
