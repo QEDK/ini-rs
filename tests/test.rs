c94ad23a94fbbe0e1d634cdc5a1b079bf5ea3e92
ankit <1994constant@gmail.com>
2020-06-20 02:37:56 +0530

    Update tests

--- tests/test.rs
+++ tests/test.rs
@@ -6,5 +6,24 @@ fn main() {
 	let map1 = ini!(safe "tests/test.ini");
 	let (map2, map3) = ini!("tests/test.ini", "tests/test.ini");
 	assert_eq!(assert_eq!(map2, map3), assert_eq!(map3, map1.clone().unwrap()));
+	let instring = "[values]
+	key = value
+
+		[VALUES2]
+			key2=value2
+
+	[section]
+	key = 42";
+	let map4 = inistr!(safe "[values]
+	key = value
+
+		[VALUES2]
+			key2=value2
+
+	[section]
+	key = 42");
+	let (map5, map6) = inistr!(&String::from(instring), instring);
+	assert_eq!(assert_eq!(map5, map6), assert_eq!(map5, map4.clone().unwrap()));
+	assert_eq!(assert_eq!(map2, map5), assert_eq!(map1, map4));
 	assert_eq!(map1.unwrap()["section"]["key"].clone().unwrap(), "42");
 }
